6c273de1f4de5512f66a671d87f00f23416679bd
Prachi-Mahajan <prachimahajan59@gmail.com>
2020-09-29 11:51:40 +0530

    fhi

---
+++ Assignment 5/index.php
@@ -0,0 +1,18 @@
+
+<title>Prachi Mahajan</title>
+<h1>Prachi Mahajan PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Prachi Mahajan\" is ".hash('sha256', 'Prachi Mahajan');
+?></p>
+<pre>ASCII ART:
+********
+*       *
+*  *****
+*  *
+****    
+        
+    
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
